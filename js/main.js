2702cd39702710d897e834898c20325b29561e34
anami823 <anami823@gmail.com>
2019-08-09 16:37:27 -0700

    Finalize

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
-/*----- constants -----*/ 
-/*----- app's state (variables) -----*/ 
-/*----- cached element references -----*/ 
-/*----- event listeners -----*/ 
-/*----- functions -----*/
+// add event listener new-skill
+let skill;
+
+const getSkill = (evt)=> {
+    skill = evt.target.value
+    evt.target.value  = ''
+}
+
+const submitSkill = (evt) => {
+    $('ul').append(
+        `<li>
+        <button class="delete" type="button">X</button><span>${skill}</span>          
+        </li>`) 
+}
+
+const deleteSkill = (evt) => {
+    $(evt.target).parent().html('')
+    console.log(this)
+    console.log($(evt.target).parent() )
+}
+
+$('#add-skill-btn').on('click', submitSkill)
+$('#skill-text-box').on('blur', getSkill)
+$('ul').on('click', deleteSkill)
+//capture input in text box
+
+//on click add skill to UL
+
 
 
 
